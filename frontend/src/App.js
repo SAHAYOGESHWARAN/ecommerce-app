1d5cf45aa0ab214c78127345c86904dfa6e4b435
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-10 17:30:25 +0530

    E-app'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Login from './components/Auth/Login'; // Ensure case sensitivity
-import Register from './components/Auth/Register'; // Ensure case sensitivity
-import ProductList from './components/Product/ProductList'; // Ensure case sensitivity
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import ProductList from './components/Product/ProductList';
 import ProductDetails from './components/Product/ProductDetails';
 import ProductUpload from './components/Product/ProductUpload';
+import Navbar from './components/Navbar'; // Importing Navbar
 
 const App = () => {
   return (
     <Router>
+      <Navbar /> {/* Adding the Navbar */}
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
