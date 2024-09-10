8fad85e7be9c962340aef7318229a42c3d60f1fb
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-10 16:49:47 +0530

    E-app

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Products</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product._id}>{product.name} - ${product.price}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
