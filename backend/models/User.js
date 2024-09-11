1e1a2963b6d254bcbafa2c5ee98eee5f704c9deb
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-11 12:47:25 +0530

    EA

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  googleId: String,
-  name: String,
-  email: String
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', UserSchema);
