0015bd1eb6c2a934dfd656f0f0f26488d39f216f
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-11 17:06:35 +0530

    EA

---
+++ frontend/src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'; // Import CSS file for styling
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>© sahayogeshwaran .</p>
+    </footer>
+  );
+};
+
+export default Footer;
