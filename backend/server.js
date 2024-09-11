2f24fa1d661bc0737ab9196ed7e0c2f8ba3485f1
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-11 12:18:48 +0530

    EA

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const authRoutes = require('./routes/auth');
+const authRoutes = require('./routes/authRoutes');
 const app = express();
 require('dotenv').config();
 
