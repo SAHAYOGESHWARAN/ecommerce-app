693a523e0d3d56758b2acb80aaf613f94e2469b0
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-11 12:48:07 +0530

    EA

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -26,29 +26,7 @@ exports.register = async (req, res) => {
     // Send response
     res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
-    res.status(500).json({ message: 'Server error' });
-  }
-};
-
-// User login
-exports.login = async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(400).json({ message: 'User not found' });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ message: 'Invalid credentials' });
-    }
-
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.json({ token });
-
-  } catch (error) {
+    console.error('Registration error:', error);
     res.status(500).json({ message: 'Server error' });
   }
 };
