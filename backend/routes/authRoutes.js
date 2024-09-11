e01a7479009cc762fadb2aa579d108eac89ad9c5
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-11 12:37:50 +0530

    EA

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,13 +1,11 @@
 const router = require('express').Router();
 const passport = require('passport');
 
-// Auth Routes
 router.get('/google', passport.authenticate('google', {
   scope: ['profile', 'email']
 }));
 
 router.get('/google/callback', passport.authenticate('google'), (req, res) => {
-  // Successful authentication, redirect home.
   res.redirect('/home'); // Adjust the redirect URI as needed
 });
 
